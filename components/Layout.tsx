bb0cb20363b4986933871198e15a7849ac4650ca
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-01-11 18:58:06 -0500

    favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,6 +16,7 @@ const Layout = ({ children, title = "Bryan Murphy" }: Props) => (
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <Header />
       {children}
