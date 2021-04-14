cda4b847c0472d77d84ac67710a25fb2ec3ec573
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2021-04-14 11:01:12 -0400

    about view mobile tweaks, env var additions

--- frontend/components/views/AboutView.tsx
+++ frontend/components/views/AboutView.tsx
@@ -14,34 +14,33 @@ type Props = {
 };
 
 Modal.setAppElement("#__next");
-Modal.defaultStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(255, 255, 255, 0.75)",
-  },
-  content: {
-    position: "absolute",
-    top: "5px",
-    left: "5px",
-    right: "5px",
-    bottom: "5px",
-    border: "1px solid #ccc",
-    background: "#fff",
-    overflow: "auto",
-    WebkitOverflowScrolling: "touch",
-    borderRadius: "4px",
-    outline: "none",
-    // padding: "20px",
-  },
-};
+// Modal.defaultStyles = {
+//   overlay: {
+//     position: "fixed",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     backgroundColor: "rgba(255, 255, 255, 0.75)",
+//   },
+//   content: {
+//     position: "absolute",
+//     top: "5px",
+//     left: "5px",
+//     right: "5px",
+//     bottom: "5px",
+//     border: "1px solid #ccc",
+//     background: "#fff",
+//     overflow: "auto",
+//     WebkitOverflowScrolling: "touch",
+//     borderRadius: "4px",
+//     outline: "none",
+//     // padding: "20px",
+//   },
+// };
 
 const AboutView = ({ about }: Props) => {
   const [open, setOpen] = useState(false);
-  const handleModalClose = () => setOpen(false);
 
   const handleOpenModal: any = () => {
     setOpen(true);
@@ -49,7 +48,7 @@ const AboutView = ({ about }: Props) => {
   return (
     <>
       <div className="grid grid-cols-12">
-        <div className="col-start-4 col-span-6">
+        <div className="col-start-2 col-span-10 md:col-start-4 md:col-span-6">
           <Image
             src={getStrapiMedia(about.profilePic)}
             layout="responsive"
@@ -57,7 +56,9 @@ const AboutView = ({ about }: Props) => {
             width={about.profilePic.width}
           />
         </div>
-        <p className="col-start-4 col-span-6 text-xl my-16">{about.body}</p>
+        <p className="col-start-2 col-span-10 md:col-start-4 md:col-span-6 text-xl my-16">
+          {about.body}
+        </p>
 
         <Button
           className="col-start-6 col-span-2 mx-auto"
@@ -65,25 +66,14 @@ const AboutView = ({ about }: Props) => {
         >
           Resume
         </Button>
-        <Modal isOpen={open} onRequestClose={handleModalClose}>
-          {/* <Document
-            // className="mx-auto"
-            file={getStrapiURL(about.resume.url)}
-            >
-            <Page
-            // className="mx-auto"
-            // width={250}
-            // scale={0.75}
-            // renderMode="svg"
-            pageNumber={1}
-            />
-          </Document> */}
+        <Modal isOpen={open}>
           <Image
             src={getStrapiMedia(about.resume)}
             height={about.resume.height}
             width={about.resume.width}
+            layout="responsive"
           />
-          <Button className="w-full mx-auto" onClick={() => setOpen(false)}>
+          <Button className="w-1/4 mx-auto" onClick={() => setOpen(false)}>
             Close
           </Button>
         </Modal>
