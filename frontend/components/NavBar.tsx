9d548f554f5b4a061ed0b084cf8ab220488228ad
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-04-14 15:10:05 -0400

    project detail mobile styles

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -9,13 +9,13 @@ type Props = {
 const NavBar = ({ className = "" }: Props) => {
   return (
     <Nav className={`${className} flex justify-center items-center`}>
-      <Nav.Item className="px-12">
+      <Nav.Item className="px-5 md:px-12">
         <Link href={"/"}>projects</Link>
       </Nav.Item>
-      <Nav.Item className="px-12">
+      <Nav.Item className="px-5 md:px-12">
         <Link href={"/"}>about</Link>
       </Nav.Item>
-      <Nav.Item className="px-12">
+      <Nav.Item className="px-5 md:px-12">
         <Link href={"/"}>contact</Link>
       </Nav.Item>
     </Nav>
